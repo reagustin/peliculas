3127f433345d3fea3d19f157e01829e0b2c76777
Agustin Re <re_agustin@hotmail.com>
2021-02-24 20:38:57 -0300

    first commit

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+
+class DataSearch extends SearchDelegate {
+  String seleccion = '';
+  final peliculasProvider = new PeliculasProvider();
+  final peliculas = [
+    'Spiderman',
+    'Aquaman',
+    'Superman',
+    'Eschiman',
+    'Shazam!',
+    'Ironman',
+    'Capitan America'
+  ];
+
+  final peliculasRecientes = [
+    'Spiderman',
+    'Capitan America',
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro appbar
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda de nuestro appbar
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // crea lo resultados que va a mostrar
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugerencias al usuario cuando escribe en el search
+
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        if (snapshot.hasData) {
+          final peliculas = snapshot.data;
+          return ListView(
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  close(context, null);
+                  pelicula.uniqueId = '';
+                  Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                },
+              );
+            }).toList(),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+// @override
+//   Widget buildSuggestions(BuildContext context) {
+//     // Sugerencias al usuario cuando escribe en el search
+
+//     final listaSugerida = (query.isEmpty)
+//         ? peliculasRecientes
+//         : peliculas
+//             .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+//             .toList();
+
+//     return ListView.builder(
+//       itemCount: listaSugerida.length,
+//       itemBuilder: (context, i) {
+//         return ListTile(
+//           leading: Icon(Icons.movie),
+//           title: Text(listaSugerida[i]),
+//           onTap: () {
+//             seleccion = listaSugerida[i];
+//             showResults(context);
+//           },
+//         );
+//       },
+//     );
+//   }
+
+}
